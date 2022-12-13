c8a5096a4b26bd2c06ecadf49ae22b702714c19e
BMayowa <mayowaobi74@gmail.com>
2022-12-13 16:32:37 +0100

    Python Webcam manipulation using OpenCV

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2 as cv
 
 cap = cv.VideoCapture(0)
@@ -6,14 +5,11 @@ if not cap.isOpened():
     print("Cannot open Camera")
     exit()
 
-print([cap.get(i) for i in range(18)])
-# print(cv.__dir__())
-
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Flip the webcam
+    # The webcam display is not mirrored, flip to simulate mirroring.
     frame = cv.flip(frame, 1)
 
     # if frame is read correctly, ret is True
@@ -21,13 +17,15 @@ while True:
         print("Cannot receive frame (stream end?). Exiting...")
         break
 
-    # Operations on the frame happen here
+    # Operations on the frame happen below
+
+    # To show the output as grayscale
     # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
+    # Display the resulting grayscale frame
     # cv.imshow('frame', gray)
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
-        print("")
+        print("Letter Q has been pressed. I will exit now.")
         break
 
 # When everything done, release the capture
